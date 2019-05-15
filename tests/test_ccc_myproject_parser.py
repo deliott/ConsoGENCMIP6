f331dfb06d8c84141ac6523e65f1b5085f62e789
deliott <eliott.dupont@laposte.net>
2019-05-15 17:33:30 +0200

    addition of the use of the methods :    - self.set_path_to_raw_data()    - self.get_list_of_possible_files_to_parse()
       to the init method
    
    Changes made accordingly in the test_suite

--- tests/test_ccc_myproject_parser.py
+++ tests/test_ccc_myproject_parser.py
@@ -11,10 +11,17 @@ class Test_CCC_MYPROJECT_Parser(TestCase):
     #     # self.path = '../tests/config_test.ini/'
     #     self.path = '/home/edupont/ccc_myproject_data/mock_ccc_myproject.log'
 
-    def test_parser_init(self):
+    """def test_parser_init(self):
         parser = Parser()
         self.assertEqual(parser.path_to_raw_data, "/default/path/")
         self.assertEqual(parser.list_of_possible_files_to_parse, [])
+
+        This was deleted du to the addition of the use of the methods
+        self.set_path_to_raw_data()
+        self.get_list_of_possible_files_to_parse()
+
+        in the __init__() of the Parser class
+    """
 
         # self.assertEqual(parser.number_of_projects, 0)
     # def test_parse_config(self):
@@ -23,7 +30,7 @@ class Test_CCC_MYPROJECT_Parser(TestCase):
 # @TODO : test if the fqdn does not give the answers expected
     def test_set_path_to_raw_data(self):
         parser = Parser()
-        parser.set_path_to_raw_data()
+        #parser.set_path_to_raw_data()
         self.assertEqual(parser.path_to_raw_data[-20:], "/ccc_myproject_data/")
 
 # @TODO: test the opening is done properly using mock library in unittest.
@@ -38,14 +45,14 @@ class Test_CCC_MYPROJECT_Parser(TestCase):
         mock_listdir.return_value = ['ccc_myproject_20190515.log', 'ccc_myproject_20190514',
                                      'ccc_myproject_20190515.dat', 'c.json', 'd.txt']
         parser = Parser()
-        parser.get_list_of_possible_files_to_parse()
+        #parser.get_list_of_possible_files_to_parse()
         files = parser.list_of_possible_files_to_parse
         self.assertEqual(1, len(files))
 
         # test if the begining of the name properly taken into account
         mock_listdir.return_value = ['ccc_myproject.log', 'ccc_myproject_20190515.log', 'b.json']
         parser = Parser()
-        parser.get_list_of_possible_files_to_parse()
+        #parser.get_list_of_possible_files_to_parse()
         files = parser.list_of_possible_files_to_parse
         self.assertEqual(1, len(files))
 
