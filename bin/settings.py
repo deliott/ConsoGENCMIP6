816af9ae5738979d32cb2d9dffeca899e8b31a9a
deliott <eliott.dupont@laposte.net>
2019-05-15 17:24:06 +0200

    added global variable path_to_ccc_myproject_raw_data
    used by ccc_myproject_parser.py

--- bin/settings.py
+++ bin/settings.py
@@ -3,5 +3,5 @@
 def init():
     global config_path
     config_path = "\defaul\config\path"
-    #"/home/edupont/Documents/mesocentre/ConsoGENCMIP6_git/ConsoGENCMIP6/bin/config_conso_local.ini"
-
+    global path_to_ccc_myproject_raw_data
+    path_to_ccc_myproject_raw_data = "\defaul\config\path"
