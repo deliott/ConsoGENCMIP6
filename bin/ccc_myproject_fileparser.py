3d4002acb1c40d37d18b02ceb4c6562decb2a292
deliott <eliott.dupont@laposte.net>
2019-05-20 13:42:47 +0200

    Improved doctrings and test. Mocks can be used to test more methods.

--- bin/ccc_myproject_fileparser.py
+++ bin/ccc_myproject_fileparser.py
@@ -4,46 +4,33 @@ import os
 import shutil
 
 
-class FileParser():
+class FileParser:
 
     def __init__(self):
-        """
-        :param path_to_file: absolute path of the file that will be parsed, here it is the last one.
-        """
+        # """
+        # :param path_to_file: absolute path of the file that will be parsed, here it is the last one.
+        # """
         parser = Parser()
         # parser = ccc_myproject_parser.Parser()
         parser.set_path_to_raw_data()
         parser.get_list_of_possible_files_to_parse()
-
         # print(parser.path_to_raw_data)
         # print(parser.list_of_possible_files_to_parse)
-
         self.path_to_file = parser.path_to_raw_data + parser.list_of_possible_files_to_parse[-1]
 
     def __init__(self,path_to_one_file_to_parse):
         """
-        :param path_to_file: absolute path of the file that will be parsed, here it is the last one.
+        :param path_to_one_file_to_parse: absolute path of the file that will be parsed, here it is the last one.
         """
-
         self.path_to_file = path_to_one_file_to_parse
         self.project_name = ""
 
     def get_project_name(self):
-        """method to get the name of the project from the raw log of ccc_myproject"""
-
-    # with open( self.path_to_file, "r") as filein:
-    #     # Skip lines until we find project name.
-    #     # Then extract script date
-    #     for ligne in filein:
-    #       if "Accounting" in ligne:
-    #         today = ligne.split()[-1]
-    #         today = string_to_date(today)
-    #         project["machine"] = ligne.split()[5]
-    #         project["nodes"]   = ligne.split()[6]
-    #         break
+        """Get the name of the project from the raw log of ccc_myproject."""
+        pass
 
     def get_project_last_line(self):
-        """Method to get a list of indexes of the lines after the deadlinesof a project are told"""
+        """Get a list of indexes of the lines after the deadlines of a project are told"""
         end_of_project_line_list = []
 
         with open(self.path_to_file, "r") as filein:
@@ -52,12 +39,11 @@ class FileParser():
                 line_number = line_number + 1
                 if "Project deadline" in ligne:
                     end_of_project_line_list.append(line_number + 1)
-
         return end_of_project_line_list
 
     def set_path_to_individual_projects_directory(self):
         """
-        Determines the path, and the name of the directory where the splitted ccc_myproject files will be written.
+        Determine the path, and the name of the directory where the splitted ccc_myproject files will be written.
 
         :return path: path to the directory where to store temp splitted ccc_myproject data
         :rtype path: str
@@ -65,16 +51,15 @@ class FileParser():
 
         splitted_path = self.path_to_file.split('/')[:-1]
         new_folder_name = self.path_to_file.split('/')[-1][:-4]
-        # add the name of the new directory
-        # splitted_path.append(new_folder_name[-1][:-4])
+        # Add the name of the new directory
         splitted_path.append(new_folder_name)
-        # reconstruct the path to the new directory
+        # Reconstruct the path to the new directory
         path = '/'.join(splitted_path)
         return path
 
     def create_individual_projects_directory(self):
         """
-        Method to create the directory to store the project splitted ccc_myproject files.
+        Create the directory to store the project splitted ccc_myproject files.
         Not tested.
         :return: None
         """
@@ -85,7 +70,7 @@ class FileParser():
 
     def delete_individual_projects_directory(self):
         """
-        Method to delete the directory to store the project splitted ccc_myproject files.
+        Delete the directory to store the project splitted ccc_myproject files. If it exists.
         Not tested.
         :return: None
         """
@@ -93,15 +78,17 @@ class FileParser():
         if os.path.isdir(dirpath):
             shutil.rmtree(dirpath)
 
-
     def copy_project_from_raw_input(self):
-
+        """
+        Split the raw ccc_my_project data into subfiles, one per project
+        Not tested.
+        :return: None
+        """
         beginning_of_project_line = 0
         project_number = 0
-
         end_of_project_line = self.get_project_last_line()
 
-        f = open(self.path_to_file,"r")
+        raw_ccc_myproject_file = open(self.path_to_file, "r")
         while len(end_of_project_line) > 0:
             project_number = project_number + 1
             print(end_of_project_line)
@@ -109,44 +96,26 @@ class FileParser():
 
             path_to_new_file = self.set_path_to_individual_projects_directory() + '/project_' + \
                                str(project_number) + '.log'
-            w = open(path_to_new_file, "wt")
+            project_specific_file = open(path_to_new_file, "wt")
 
-            for ligne in range(beginning_of_project_line,end_line):
-                line_to_copy = f.readline()
-                w.write(str(line_to_copy))
-
-
-
-# def __init__(self, date):
-    #     """
-
-    #     :param path_to_file: absolute path of the file that will be parsed, here it is the one indicated by the date.
-    #     """
-    #     self.path_to_file = self.path_to_raw_data + self.get_list_of_possible_files_to_parse()[0]
+            for ligne in range(beginning_of_project_line, end_line):
+                line_to_copy = raw_ccc_myproject_file.readline()
+                project_specific_file.write(str(line_to_copy))
 
 
-
-
-if __name__ == "__main__":
-
-    print("\nBeginning of execution\n")
-
-    # file_to_parse = FileParser('/home/edupont/ccc_myproject_data/mock_ccc_myproject.log')
-    file_to_parse = FileParser('/home/edupont/ccc_myproject_data/ccc_myproject_20190514.log')
-
-
-    print(file_to_parse.path_to_file)
-    print(file_to_parse.get_project_last_line())
-    liste = file_to_parse.path_to_file.split('/')[:-1]
-
-
-    print(file_to_parse.set_path_to_individual_projects_directory())
-
-    file_to_parse.create_individual_projects_directory()
-    file_to_parse.copy_project_from_raw_input()
-    # number = 3
-    # print(file_to_parse.path_to_file[:(-4)] + '/project_'+ str(number) + '.log')
-    # file_to_parse.copy_project_from_raw_imput()
-    print("\nEnd of execution\n")
-
-
+# if __name__ == "__main__":
+#
+#     print("\nBeginning of execution\n")
+#
+#     # file_to_parse = FileParser('/home/edupont/ccc_myproject_data/mock_ccc_myproject.log')
+#     file_to_parse = FileParser('/home/edupont/ccc_myproject_data/ccc_myproject_20190514.log')
+#     print(file_to_parse.path_to_file)
+#     print(file_to_parse.get_project_last_line())
+#
+#     liste = file_to_parse.path_to_file.split('/')[:-1]
+#     print(file_to_parse.set_path_to_individual_projects_directory())
+#
+#     file_to_parse.create_individual_projects_directory()
+#     file_to_parse.copy_project_from_raw_input()
+#
+#     print("\nEnd of execution\n")
--- tests/test_file_parserv2.py
+++ tests/test_file_parserv2.py
@@ -26,13 +26,22 @@ class TestFileParser(TestCase):
         liste1 = fileparser1.get_project_last_line()
         self.assertEqual(liste1 ,[57, 123] )
 
+        fileparser2 = FileParser('/home/edupont/ccc_myproject_data/ccc_myproject_20190514.log')
+        liste2 = fileparser2.get_project_last_line()
+        self.assertEqual(liste2 ,[2984, 3050] )
+
         # fileparser2 = FileParser('path')
         # liste2 = fileparser2.get_project_last_line()
         # self.assertEqual(liste2, [])
 
+    def test_set_path_to_individual_projects_directory(self):
+        fileparser1 = FileParser('/home/edupont/ccc_myproject_data/mock_ccc_myproject.log')
+        self.assertEqual(fileparser1.set_path_to_individual_projects_directory(),\
+                         '/home/edupont/ccc_myproject_data/mock_ccc_myproject')
 
-
-
+        fileparser2 = FileParser('/home/eldupont/ccc_myproject_data/ccc_myproject_20190514.log')
+        self.assertEqual(fileparser2.set_path_to_individual_projects_directory(), \
+                         '/home/eldupont/ccc_myproject_data/ccc_myproject_20190514')
 
 
 if __name__ == '__main__':
