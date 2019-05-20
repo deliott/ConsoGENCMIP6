0ea8757678f006757dbe3110b469bfa70266aba1
deliott <eliott.dupont@laposte.net>
2019-05-20 16:42:36 +0200

    Added Exceptions if file openning errors. Added closing of the openned files.

--- bin/ccc_myproject_fileparser.py
+++ bin/ccc_myproject_fileparser.py
@@ -18,16 +18,12 @@ class FileParser:
         # print(parser.list_of_possible_files_to_parse)
         self.path_to_file = parser.path_to_raw_data + parser.list_of_possible_files_to_parse[-1]
 
+
     def __init__(self,path_to_one_file_to_parse):
         """
         :param path_to_one_file_to_parse: absolute path of the file that will be parsed, here it is the last one.
         """
         self.path_to_file = path_to_one_file_to_parse
-        self.project_name = ""
-
-    def get_project_name(self):
-        """Get the name of the project from the raw log of ccc_myproject."""
-        pass
 
     def get_project_last_line(self):
         """Get a list of indexes of the lines after the deadlines of a project are told"""
@@ -88,34 +84,44 @@ class FileParser:
         project_number = 0
         end_of_project_line = self.get_project_last_line()
 
-        raw_ccc_myproject_file = open(self.path_to_file, "r")
+        try :
+            raw_ccc_myproject_file = open(self.path_to_file, "r")
+        except IOError:
+            print("Failed to open " + self.path_to_file + " file in copy_project_from_raw_input. ")
         while len(end_of_project_line) > 0:
             project_number = project_number + 1
-            print(end_of_project_line)
+            # print(end_of_project_line)
             end_line = end_of_project_line.pop(0)
 
             path_to_new_file = self.set_path_to_individual_projects_directory() + '/project_' + \
                                str(project_number) + '.log'
-            project_specific_file = open(path_to_new_file, "wt")
+            try :
+                project_specific_file = open(path_to_new_file, "wt")
+            except  IOError:
+                print("Failed to open " + path_to_new_file +  " file in copy_project_from_raw_input. " )
 
             for ligne in range(beginning_of_project_line, end_line):
                 line_to_copy = raw_ccc_myproject_file.readline()
                 project_specific_file.write(str(line_to_copy))
+            project_specific_file.close()
+
+        raw_ccc_myproject_file.close()
 
 
-# if __name__ == "__main__":
-#
-#     print("\nBeginning of execution\n")
-#
-#     # file_to_parse = FileParser('/home/edupont/ccc_myproject_data/mock_ccc_myproject.log')
-#     file_to_parse = FileParser('/home/edupont/ccc_myproject_data/ccc_myproject_20190514.log')
-#     print(file_to_parse.path_to_file)
-#     print(file_to_parse.get_project_last_line())
-#
-#     liste = file_to_parse.path_to_file.split('/')[:-1]
-#     print(file_to_parse.set_path_to_individual_projects_directory())
-#
-#     file_to_parse.create_individual_projects_directory()
-#     file_to_parse.copy_project_from_raw_input()
-#
-#     print("\nEnd of execution\n")
+
+if __name__ == "__main__":
+
+    print("\nBeginning of execution\n")
+
+    # file_to_parse = FileParser('/home/edupont/ccc_myproject_data/mock_ccc_myproject.log')
+    file_to_parse = FileParser('/home/edupont/ccc_myproject_data/ccc_myproject_20190514.log')
+    print(file_to_parse.path_to_file)
+    print(file_to_parse.get_project_last_line())
+
+    liste = file_to_parse.path_to_file.split('/')[:-1]
+    print(file_to_parse.set_path_to_individual_projects_directory())
+
+    file_to_parse.create_individual_projects_directory()
+    file_to_parse.copy_project_from_raw_input()
+
+    print("\nEnd of execution\n")
