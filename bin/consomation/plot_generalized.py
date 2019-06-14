ebad07387791e9c3eb4c4975c273bb69d1116ab1
deliott <eliott.dupont@laposte.net>
2019-06-14 14:09:54 +0200

    Refactored the plot rendering into different functions. No tests.

--- bin/consomation/plot_generalized.py
+++ bin/consomation/plot_generalized.py
@@ -6,21 +6,17 @@ from bin.consomation.data_for_plot_extractor import ProjectData
 
 
 
-# processor = 'Skylake'
-# project_name = 'gencmip6'
-# data_for_plot = ProjectData('gencmip6')
-# df_data, df_opti = data_for_plot.run_data_for_plot_extractor('Skylake', '2019-05-01')
-
-#
-# processor = 'KNL'
 processor = 'Skylake'
-project_name = 'gen0826'
-data_for_plot = ProjectData(project_name)
-df_data, df_opti = data_for_plot.run_data_for_plot_extractor(processor, '2018-10-31')
+project_name = 'gencmip6'
+data_for_plot = ProjectData('gencmip6')
+df_data, df_opti = data_for_plot.run_data_for_plot_extractor('Skylake', '2019-05-01')
+#
 
-
-
-
+# processor = 'KNL'
+# # processor = 'Skylake'
+# project_name = 'gen0826'
+# data_for_plot = ProjectData(project_name)
+# df_data, df_opti = data_for_plot.run_data_for_plot_extractor(processor, '2018-10-31')
 
 #######################################################################################
 #######################################################################################
@@ -37,167 +33,42 @@ from bokeh.palettes import Spectral
 import bin.consomation.settings as settings
 import bin.consomation.set_paths as set_paths
 
+import bin.consomation.plot_set_up as plot_set_up
 
 set_paths.set_path_to_plots()
 
 
 #############################
 # Configuration du Plot :
+p = plot_set_up.plot_init(processor, project_name)
 
-source = ColumnDataSource(df_data)
-p = figure(title="Consommation de l'allocation " + project_name.swapcase() + " - Vue par MIPs/sous-projets",
-           x_axis_label="Date",
-           y_axis_label="Irene " + processor + " (heures)",
-           x_axis_type="datetime",
-           plot_width=1800, plot_height=800,
-           sizing_mode='scale_width'
-           )
-
+# Ajout des glyphs à la liste
+nb_sousprojets = 10
 line_list = []
 
-# Ajout des lignes des sous projets au plot :
-# Nb de sous projets à ploter :
-nb_sousprojets = 10
-nb_plot = 1
+# line_list = plot_set_up.add_subprojects_to_line_list(nb_sousprojets, df_data, p, line_list)
+plot_set_up.add_subprojects_to_line_list(nb_sousprojets, df_data, p, line_list)
 
-palette = list(reversed(Spectral[nb_sousprojets]))
-
-for header in list(df_data.columns):
-    # print(header)
-    if not header == 'Date':
-        # Conditions pour afficher le sous projet :
-            # sa dernière valeure n'est pas nulle
-            # On pourrait rajouter d'autres conditions, telles que :
-                # - si la valeur a varié depuis longtemps.
-        if not df_data[header].iloc[-1] == 0:
-            if nb_plot < nb_sousprojets:
-                if header == 'Total':
-                    line_list.append(p.line('Date', header, source=source,
-                           legend=header + ' ', # small hack to be able to display the name. Otherwise, without the ' ' there is a bug
-                           name=header + ' ', # small hack to be able to display the name. Otherwise, without the ' ' there is a bug
-                           line_width=3,
-                           color="black",
-                           muted_color="black", muted_alpha=0.2
-                           )
-                                     )
-                else:
-                    line_list.append(p.line('Date', header, source=source,
-                           legend=header + ' ',
-                           name=header + ' ',
-                           # small hack to be able to display the name. Otherwise, without the ' ' there is a bug
-                           line_width=3,
-                           color=palette[nb_plot],
-                           muted_color=palette[nb_plot], muted_alpha=0.2
-                           )
-                                     )
-                    nb_plot = nb_plot + 1
+plot_set_up.add_optimal_consumption_curve(df_opti, p, line_list)
 
 
+# Ajout de la surface de sécurité de consomation théorique :
+
+delai_avant_penalite=60
+plot_set_up.add_optimal_consumption_patch(delai_avant_penalite, df_opti, p, 'orange')
+
+# print('Last Total : ', df_data['Total'].iloc[-1])
+
+# Ajout des ticks de difference entre Conso Optimale et Totale
+days_in_advance = ProjectData.days_in_advance
+plot_set_up.add_optimal_total_difference_ticks(df_data, df_opti, p, days_in_advance)
 
 
+# Ajout du HoverTool pour les courbes de données
 
-# Ajout de la courbe de consomation théorique :
-source_opt = ColumnDataSource(df_opti)
-line_list.append(
-    p.line('Date', 'Conso_Optimale', source=source_opt,
-                           legend='Conso_Optimale ',
-                            name='Conso_Optimale ',
-                           # small hack to be able to display the name. Otherwise, without the ' ' there is a bug
-                           line_width=1,
-                           color='black',
-                           muted_color='black', muted_alpha=0.2
-                           )
-)
+plot_set_up.add_curves_hovertool(p, line_list)
 
-# Ajout de la courbe de consomation théorique :
-# delai_avant_penalite = 14
-delai_avant_penalite = 60
-volume_avant_penalite = df_opti['Conso_Optimale'][delai_avant_penalite]
-
-xx = [df_opti['Date'][0],
-      df_opti['Date'][-1],
-      df_opti['Date'][-1],
-      df_opti['Date'][delai_avant_penalite],
-      df_opti['Date'][0]]
-
-yy = [df_opti['Conso_Optimale'][0],
-      df_opti['Conso_Optimale'][-1],
-      df_opti['Conso_Optimale'][-1] - volume_avant_penalite,
-      0,
-      0]
-p.patch(xx, yy, alpha=0.2, line_width=2)
-
-# Ajout de la difference Optimale/Réelle:
-
-print(df_data['Total'].iloc[-1])
-
-last_opti = df_opti['Conso_Optimale'][-(ProjectData.days_in_advance + 1)]
-last_real = df_data['Total'].iloc[-1]
-last_date = df_data['Date'].iloc[-1]
-deltaConso = '{:,.0f}'.format(abs(last_opti - last_real)) + ' heures'
-
-
-if last_opti > last_real:
-    statut = 'Retard'
-    statut_color = 'red'
-    statut_top = last_opti
-    statut_bottom = last_real
-else:
-    statut = 'Avance'
-    statut_color = 'green'
-    statut_top = last_real
-    statut_bottom = last_opti
-
-
-source = ColumnDataSource(dict(
-        left=[last_date],
-        top=[statut_top],
-        right=[last_date + datetime.timedelta(days=0.01)],
-        bottom=[statut_bottom],
-        color=[statut_color],
-    )
-)
-retard_warning = p.quad(left="left", right="right", top="top", bottom="bottom",
-              color=None,
-              hover_color="color",
-              source=source,
-              alpha=0.3,
-              name=deltaConso
-                        )
-p.add_tools(HoverTool(
-    tooltips=[
-                (statut, '$name'),
-                # ('Courbe', '$name'),
-                # ('Hours', '$y{0,2f}'),  # use @{ } for field names with spaces
-            ],
-    renderers=[retard_warning],
-    mode='hline'))
-
-# Ajout du HoverTool
-p.add_tools(HoverTool(
-    renderers=line_list,
-    tooltips=[
-        ('Date', '$x{%F}'),
-        ('Courbe', '$name'),
-        ('Hours', '$y{0,2f}'),  # use @{ } for field names with spaces
-        ],
-    formatters={
-        '$x': 'datetime',  # use 'datetime' formatter for 'date' field
-        },
-    mode='mouse'
-                    )
-            )
-
-# Ajout de quelsues paramètres pour le graph
-
-p.legend.location = "top_left"
-p.legend.click_policy = "mute"
-
-p.xaxis[0].formatter.days = '%a - %d/%m/%Y'
-p.xaxis.major_label_orientation = pi / 3
-p.yaxis.formatter = NumeralTickFormatter(format="0,")
-
-
+plot_set_up.plot_config(p)
 
 output_file(settings.path_to_plots + "/" + project_name + "_mips_timeseries.html", title= project_name + " mips timeseries")
 
---
+++ bin/consomation/plot_set_up.py
@@ -0,0 +1,233 @@
+
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
+from bokeh.palettes import Spectral
+
+import datetime
+from math import pi
+
+# from bin.consomation.data_for_plot_extractor import ProjectData.days_in_advance
+
+
+def plot_init(processor, project_name):
+    """
+    Initialize the bokeh plot
+    :param processor: str name of the processor type whose data will be plotted
+    :param project_name: str, name of the project plotted
+    :return p: bokeh figure
+    """
+    p = figure(title="Consommation de l'allocation " + project_name.swapcase() + " - Vue par MIPs/sous-projets",
+               x_axis_label="Date",
+               y_axis_label="Irene " + processor + " (heures)",
+               x_axis_type="datetime",
+               plot_width=1800, plot_height=800,
+               sizing_mode='scale_width'
+               )
+
+    return p
+
+
+def add_subprojects_to_line_list(nb_sousprojets, df_data, p, line_list):
+    """
+    Append Bokeh Line Glyphs corresponding to subprojects data to the list (line_list) to be added to the figure (p).
+
+    :param nb_sousprojets: max number of subprojects to be plotted on the figure.
+    :param df_data: dataframe with the cpu time consumption per project and total as columns. Indexed by dates.
+    :param p: bokeh figure that will render the glyphs
+    :param line_list: list with the bokeh glyphs to be added to the p figure.
+    # :return line_list: updated list of bokeh glyphs added to be added to the figure.
+    :return: None
+    """
+    nb_plot = 1
+
+    source = ColumnDataSource(df_data)
+
+    palette = list(reversed(Spectral[nb_sousprojets]))
+
+    for header in list(df_data.columns):
+        # print(header)
+        if not header == 'Date':
+            # Conditions pour afficher le sous projet :
+            # sa dernière valeure n'est pas nulle
+            # On pourrait rajouter d'autres conditions, telles que :
+            # - si la valeur a varié depuis longtemps.
+            if not df_data[header].iloc[-1] == 0:
+                if nb_plot < nb_sousprojets:
+                    if header == 'Total':
+                        if len(list(df_data.columns)) > 3:
+                            line_list.append(p.line('Date', header, source=source,
+                                                    legend=header + ' ',
+                                                    # small hack to be able to display the name.
+                                                    # Otherwise, without the ' ' there is a bug
+                                                    name=header + ' ',
+                                                    # small hack to be able to display the name.
+                                                    # Otherwise, without the ' ' there is a bug
+                                                    line_width=3,
+                                                    color="black",
+                                                    muted_color="black", muted_alpha=0.2
+                                                    )
+                                             )
+                    else:
+                        line_list.append(p.line('Date', header, source=source,
+                                                legend=header + ' ',
+                                                name=header + ' ',
+                                                # small hack to be able to display the name.
+                                                # Otherwise, without the ' ' there is a bug
+                                                line_width=3,
+                                                color=palette[nb_plot],
+                                                muted_color=palette[nb_plot], muted_alpha=0.2
+                                                )
+                                         )
+                        nb_plot = nb_plot + 1
+
+    # return line_list
+
+
+def add_optimal_consumption_curve(df_opti, p, line_list):
+    """
+    Append Bokeh Line Glyphs corresponding to the optimal consumption of the allocation to the list (line_list)
+    to be added to the figure (p).
+
+    :param df_opti: dataframe with the optimal cpu time consumption as column. Indexed by dates.
+    :param p: bokeh figure that will render the glyphs
+    :param line_list: list with the bokeh glyphs to be added to the p figure.
+    :return: None
+    """
+    # Ajout de la courbe de consomation théorique :
+    source_opt = ColumnDataSource(df_opti)
+    line_list.append(
+        p.line('Date', 'Conso_Optimale', source=source_opt,
+               legend='Conso_Optimale ',
+               name='Conso_Optimale ',
+               # small hack to be able to display the name. Otherwise, without the ' ' there is a bug
+               line_width=1,
+               color='black',
+               muted_color='black', muted_alpha=0.2
+               )
+    )
+
+
+def add_optimal_consumption_patch(delai_avant_penalite, df_opti, p, color):
+    """
+    Add Bokeh Patch Glyph to the figurte (p) corresponding to the security area
+    before risk restriction of the allocation.
+
+    :param delai_avant_penalite: Number of days before Computing Centers decides to take back part of the allocation.
+    60 Days at TGCC.
+    :param df_opti: dataframe with the optimal cpu time consumption as column. Indexed by dates.
+    :param p: bokeh figure that will render the glyphs.
+    :param color: string describing the color of the patch.
+    :return: None
+    """
+    # delai_avant_penalite = 14
+    # delai_avant_penalite = 60
+
+    if len(df_opti['Date']) < delai_avant_penalite:
+        delai_avant_penalite = min(len(df_opti['Date'])-1, 30)
+        color = 'green'
+
+    volume_avant_penalite = df_opti['Conso_Optimale'][delai_avant_penalite]
+
+    xx = [df_opti['Date'][0],
+          df_opti['Date'][-1],
+          df_opti['Date'][-1],
+          df_opti['Date'][delai_avant_penalite],
+          df_opti['Date'][0]]
+
+    yy = [df_opti['Conso_Optimale'][0],
+          df_opti['Conso_Optimale'][-1],
+          df_opti['Conso_Optimale'][-1] - volume_avant_penalite,
+          0,
+          0]
+    p.patch(xx, yy, alpha=0.1, line_width=2, legend=str(delai_avant_penalite) + ' days security area', color=color)
+
+
+def add_optimal_total_difference_ticks(df_data, df_opti, p, days_in_advance):
+    """
+    Add ticks to show difference between optimal curve and total of consumption.
+
+    :param df_data: dataframe with the cpu time consumption per project and total as columns. Indexed by dates.
+    :param df_opti: dataframe with the optimal cpu time consumption as column. Indexed by dates.
+    :param p: bokeh figure that will render the glyphs.
+    :param days_in_advance: Number of days the optimal curv
+    :return: None
+    """
+    last_opti = df_opti['Conso_Optimale'][-(days_in_advance + 1)]
+    last_real = df_data['Total'].iloc[-1]
+    last_date = df_data['Date'].iloc[-1]
+    delta_conso = '{:,.0f}'.format(abs(last_opti - last_real)) + ' heures'
+
+    if last_opti > last_real:
+        statut = 'Retard'
+        statut_color = 'red'
+        statut_top = last_opti
+        statut_bottom = last_real
+    else:
+        statut = 'Avance'
+        statut_color = 'green'
+        statut_top = last_real
+        statut_bottom = last_opti
+
+    source = ColumnDataSource(dict(
+        left=[last_date],
+        top=[statut_top],
+        right=[last_date + datetime.timedelta(days=0.01)],
+        bottom=[statut_bottom],
+        color=[statut_color],
+    )
+    )
+    retard_warning = p.quad(left="left", right="right", top="top", bottom="bottom",
+                            color=None,
+                            hover_color="color",
+                            source=source,
+                            alpha=0.3,
+                            name=delta_conso
+                            )
+    p.add_tools(HoverTool(
+        tooltips=[
+            (statut, '$name'),
+            # ('Courbe', '$name'),
+            # ('Hours', '$y{0,2f}'),  # use @{ } for field names with spaces
+        ],
+        renderers=[retard_warning],
+        # mode='vline'
+        # mode='mouse'
+        mode='hline'
+    ))
+
+
+def add_curves_hovertool(p, line_list):
+    """
+    Adds the bokeh hover tool associated to the different consomation data lines to the figure (p).
+
+    :param p:  bokeh figure that will render the glyphs
+    :param line_list: list with the bokeh glyphs to be added to the p figure.
+    :return: None
+    """
+    p.add_tools(HoverTool(
+        renderers=line_list,
+        tooltips=[
+            ('Date', '$x{%F}'),
+            ('Courbe', '$name'),
+            ('Hours', '$y{0,2f}'),  # use @{ } for field names with spaces
+        ],
+        formatters={
+            '$x': 'datetime',  # use 'datetime' formatter for 'date' field
+        },
+        mode='mouse'
+    )
+    )
+
+
+def plot_config(p):
+    """
+
+    :param p:
+    :return:
+    """
+    p.legend.location = "top_left"
+    p.legend.click_policy = "mute"
+
+    p.xaxis[0].formatter.days = '%a - %d/%m/%Y'
+    p.xaxis.major_label_orientation = pi / 3
+    p.yaxis.formatter = NumeralTickFormatter(format="0,")
