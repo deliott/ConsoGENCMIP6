4e6ab96563fc56e470a88a01a08be55bb3202c00
eliott.dupont <eliott.dupont@ipsl.fr>
2019-07-05 12:26:42 +0200

    Completed docstring and cleaned up script.

--- bokehapp/subprojects_display_methods.py
+++ bokehapp/subprojects_display_methods.py
@@ -4,13 +4,14 @@ This script contains the differents methods used by the bokeh server to display 
 """
 
 
-
-
 def get_subproject_list(source):
     """
+    Get the subproject lists separated depending on if it is active or not and sorted accordingly.
 
     :param source:
-    :return res: list of the subproject names (str) in alphabetical order
+    :return active_subprojects: list of the active subproject names (str)
+    sorted by decreasing associated last time value
+    :return inactive_subprojects: list of the inactive subproject names (str) in alphabetical order
     """
     active_subprojects = []
     inactive_subprojects = []
@@ -18,27 +19,17 @@ def get_subproject_list(source):
     df = source.to_df()
     liste = list(df.columns)
 
-    # print('\n\n\n', df, '\n\n\n')
-
     while liste:
         element = liste.pop()
         if element == 'Date' or element == 'Total':
             continue
         else:
             if df[element].iloc[-1] > 0:
-                # print('\n ', element, df[element].iloc[-1] )
                 active_subprojects.append(element)
             else:
                 inactive_subprojects.append(element)
-    # print('XXXXXXXXXXXXXZZZZZZEDZDSDCSDFVS',  active_subprojects, '\n')
-    # active_subprojects.sort()
     active_subprojects = sort_active_subprojects(active_subprojects, df)
     inactive_subprojects.sort()
-
-    # ll = active_subprojects
-    # print('Active project : ' , ll)
-    # ll.append(res0)
-    # print('All project : ' ,ll + inactive_subprojects, '\n')
 
     return active_subprojects, inactive_subprojects
 
@@ -63,11 +54,7 @@ def sort_active_subprojects(liste, df):
     last_conso_value_dict = {}
     for name in liste:
         last_conso_value_dict[name] = df[name].iloc[-1]
-    # print(last_conso_value_dict)
-
     tuple_list = sorted(last_conso_value_dict.items(), key=lambda t: t[1], reverse=True)
     result = [tuple_element[0] for tuple_element in tuple_list]
-    # print('Sorted Dict : ', result)
-
 
     return result
