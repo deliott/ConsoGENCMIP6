b2fd9b41b93f1c8e4ea251e2dff01ec0f82ef8f1
eliott.dupont <eliott.dupont@ipsl.fr>
2019-07-17 17:56:57 +0200

    Added global variable in settings: path_to_cpt_raw_data

--- bin/consomation/settings.py
+++ bin/consomation/settings.py
@@ -5,6 +5,8 @@ def init():
     config_path = "\default\config\path"
     global path_to_ccc_myproject_raw_data
     path_to_ccc_myproject_raw_data = "\default\config\path"
+    global path_to_cpt_raw_data
+    path_to_cpt_raw_data = "\default\config\path"
     global path_to_timeseries
     path_to_timeseries = "\default\\timeseries\path"
     global path_to_plots
